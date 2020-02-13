8c2645bc30a70aab789d50672e13cbae63834321
RehamAboelyazied7 <rehamaboelyazied@gmail.com>
2020-02-13 16:36:01 +0200

    Redirect to profile

--- src/main/java/com/iti/chat/viewcontroller/LoginController.java
+++ src/main/java/com/iti/chat/viewcontroller/LoginController.java
@@ -54,7 +54,7 @@ public class LoginController {
             User user = delegate.login(phoneTextField.getText(), passwordField.getText());
             if(user != null) {
                 Session.getInstance().setUser(user);
-                SceneTransition.goToHomeScene(stage);
+                SceneTransition.goToUserProfilerScene(stage);
             }
             else {
                 System.out.println("invalid phone or password");
